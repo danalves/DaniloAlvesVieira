aadcd783462f17fcb5ef542cead68903e079898f
Danilo Vieira <daniloalvesvieira@danilos-mac.local>
2018-04-17 19:45:42 -0700

    Initial Commit

---
+++ DaniloAlvesVieira/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaniloAlvesVieira
+//
+//  Created by Danilo Alves Vieira on 17/04/18.
+//  Copyright © 2018 Danilo Alves Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
